5e0de6b1470e1f89890dde96763ee08bec5456e5
svolpe43 <svolpe43@gmail.com>
2015-04-14 00:47:04 -0400

    Some UI fizes

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/var/www/gol/golms/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/var/www/gol/golms/platforms/android/ant-build/CordovaApp.ap_ \
+/var/www/gol/golms/platforms/android/ant-build/classes.dex \
+/var/www/gol/golms/platforms/android/CordovaLib/ant-build/classes.jar \
--- platforms/android/assets/www/ms.js
+++ platforms/android/assets/www/ms.js
@@ -9,32 +9,64 @@ var easy = 1;
 var medium = 2;
 var hard = 3;
 
-var boardDimWithoutLines = 240;
-var lineWidth = 5;
+var PADDING = 30;
 
+var boardWidthWithoutLines = 240;
+var boardHeightWithoutLines = 360;
+var lineWidth = 2;
+
+var horizontalCellNum;
+var verticalCellNum;
+
+var horizontalLineOffset;
+var verticalLineOffset;
+
+var gamedata;
 var gamediv
 var context;
 var board;
 var cellDim;
-var DIM;
-var lineOffset;
-var boardDimWithLines;
+var tileCheckId;
+
+var boardWidthWithLines;
+var boardHeightWithLines;
+
 var mouse;
 var bombcount;
 var bombcountspan;
+var instructionsShowing = false;
+
+var cords = {
+	x : 0,
+	y : 0
+}
 
 $(document).ready(function(){
 	gamecontainer = $("#game-container");
 	gamediv = $("#game");
 	tit = $("#title");
 	mouse = new Mouse;
-	document.onmousedown = mouseDown;
-	document.onmousemove = mouseMove;
+
+	// make sure tap doesnt get triggered by the taphold
+	$.event.special.tap.emitTapOnTaphold = false;
 	
-	$(document).bind('keyup', 'space', spaceUp);
+	// shorten the taphold duration a little default is 750ms
+	$.event.special.tap.tapholdThreshold = 400;
+
+	//document.onmousedown = mouseDown;
+	document.onmousemove = mouseMove;
+
+	$(document).on('vmousedown', function(event){
+		cords.x = event.pageX;
+		cords.y = event.pageY;
+	});
+
+	$(document).bind("tap", mouseDown);
+	$(document).bind('taphold', longPress);
+
 	bombcountspan = $("#bomb-count-text");
 
-	reset(2, false);
+	reset(2);
 });
 
 // static function to initialize an array
@@ -56,15 +88,41 @@ function Mouse(){
 	this.y = 0;
 }
 
+function GameData(cells, bombs){
+	this.numexposed = 0;
+	this.numbombs = bombs;
+	this.numcells = cells;
+	this.flagsleft = bombs;
+	this.flagsdown = 0;
+}
+
 // tile object
 function Tile(){
 	this.isBomb = false;
 	this.surrounding = 0;
 	this.state = idle;
+	this.checked = 0;
+}
+
+// show the instructions
+function instructions(){
+	if(!instructionsShowing){
+		$("#game-message").fadeIn("slow");
+		$("#instructions").show();
+		$("#game-over").hide();
+		$("#win").hide();
+		instructionsShowing = true;
+	}else{
+		$("#game-message").fadeOut("slow");
+		$("#instructions").hide();
+		$("#game-over").hide();
+		$("#win").hide();
+		instructionsShowing = false;
+	}
 }
 
 // called to reset everything basically
-function reset(difficulty, isReset){
+function reset(difficulty, isreset){
 	$("#game-message").fadeOut("slow");
 	switch(difficulty){
 		case(easy):
@@ -72,8 +130,8 @@ function reset(difficulty, isReset){
 			density = 8;
 			break;
 		case(medium):
-			cellDim = 40;
-			density = 15;
+			cellDim = 25;
+			density = 30;
 			break;
 		case(hard):
 			cellDim = 30;
@@ -81,64 +139,82 @@ function reset(difficulty, isReset){
 			break;
 	}
 
-	updateSettings(isReset);
-	randomize();
+	updateParameters(isreset);
+	randomize(20);
 	drawCells();
 }
 
 // update the settings of the game, changed when first loading and changing difficulty
-function updateSettings(isReset){
-	DIM = boardDimWithoutLines / cellDim;
-	lineOffset = (lineWidth * (DIM - 1));
-	boardDimWithLines = boardDimWithoutLines + lineOffset;
+function updateParameters(isreset){
 
-	console.log("#game dimensions: " + boardDimWithLines);
+	boardWidthWithoutLines = roundDown($(window).width() * .7, cellDim);
+	boardHeightWithoutLines = roundDown($(window).height() * .7, cellDim);
 
-	gamediv.height(boardDimWithLines);
-	gamediv.width(boardDimWithLines);
+	horizontalCellNum = boardWidthWithoutLines / cellDim;
+	verticalCellNum = boardHeightWithoutLines / cellDim;
+
+	horizontalLineOffset = (lineWidth * (horizontalCellNum - 1));
+	verticalLineOffset = (lineWidth * (verticalCellNum - 1));
+
+	boardWidthWithLines = boardWidthWithoutLines + horizontalLineOffset;
+	boardHeightWithLines = boardHeightWithoutLines + verticalLineOffset;
+
+	gamediv.height(boardHeightWithLines);
+	gamediv.width(boardWidthWithLines);
+
+	var pad = 30;
+	if(isreset)
+		pad = 0;
 	
-	$("#game-message").height(boardDimWithLines + 30);
-	$("#game-message").width(boardDimWithLines + 30);
-	$("#top").width(boardDimWithLines + 30);
+	$("#game-message").height(boardHeightWithLines + PADDING);
+	$("#game-message").width(boardWidthWithLines + PADDING);
+	$("#game-container").height(boardHeightWithLines + pad);
+	$("#game-container").width(boardWidthWithLines + pad);
+	$("#top").width(boardWidthWithLines + PADDING);
 }
 
-// randomizes the bombs on the board, this is not how the original does it
-// original uses set number of bombs and performs normal distribution
-function randomize(){
-	board = Array.matrix(DIM, DIM);
-	bombcount = 0;
-	for(var i = 0; i < DIM; i++){
-		for(var j = 0; j < DIM; j++){
-			if(Math.random() < density/100){
-				board[i][j].isBomb = true;
-				bombcount++;
-			}
+// performs random normal distribution of bombs on board
+function randomize(bombs){
+	board = Array.matrix(verticalCellNum, horizontalCellNum);
+	console.log(verticalCellNum, horizontalCellNum);
+	gamedata = new GameData(verticalCellNum * horizontalCellNum, bombs);
+	tileCheckId = 0;
+	while(bombs > 0){
+		var x = randomVal(0, verticalCellNum - 1);
+		var y = randomVal(0, horizontalCellNum - 1);
+		if(!board[x][y].isBomb){
+			board[x][y].isBomb = true;
+			bombs--;
 		}
 	}
-	bombcountspan.html(bombcount);
+	bombcountspan.html(gamedata.flagsleft);
 	countSurroundings();
 }
 
 // called when space is unpressed
-function spaceUp(event){
+function longPress(event){
 	console.log("spacedown");
-	x = getTile(mouse, false);
-	y = getTile(mouse, true);
-	if(board[x][y].state != exposed && board[x][y].state != flagged && bombcount > 0){
-		if(board[x][y].state == flagged){
-			board[x][y].state = idle;
-			bombcount++;
-		}else{
-			board[x][y].state = flagged;
-			cell = $("#cell-" + x + "-" + y);
-			cell.animate({
-			  	backgroundColor: "#008F6B",
-				color: "#fff"
-			}, 500, "easeOutBack");
-			bombcount--;
-		}
+	event.pageX = cords.x;
+	event.pageY = cords.y;
+	x = getTile(event, false);
+	y = getTile(event, true);
+	if(board[x][y].state == idle && gamedata.flagsleft > 0){
+		board[x][y].state = flagged;
+		cell = $("#cell-" + x + "-" + y);
+		cell.removeClass("surround-yes");
+		cell.addClass("flagged");
+		gamedata.flagsleft--;
+		gamedata.flagsdown++;
+	}else if(board[x][y].state == flagged){
+		board[x][y].state = idle;
+		cell = $("#cell-" + x + "-" + y);
+		cell.removeClass("flagged");
+		cell.addClass("surround-yes");
+		gamedata.flagsleft++;
+		gamedata.flagsdown--;
 	}
-	bombcountspan.html(bombcount);
+	bombcountspan.html(gamedata.flagsleft);
+	win();
 }
 
 // called when mouse is moved
@@ -153,15 +229,28 @@ function mouseDown(event){
 	x = getTile(event, false);
 	y = getTile(event, true);
 
-	if(x < 0 || y < 0 || x > DIM - 1 || y > DIM - 1)
+	if(offBoard(x, y))
 		return;
 
 	if(board[x][y].state == idle){
 		if(board[x][y].isBomb){
 			gameover();
 		}else{
+			tileCheckId++;
 			exposeTile(x, y);
 		}
+	}
+
+	win();
+}
+
+// determines if win and shows message
+function win(){
+	if(gamedata.numexposed + gamedata.flagsdown == gamedata.numcells){
+		$("#game-message").fadeIn("slow");
+		$("#game-over").hide();
+		$("#instructions").hide();
+		$("#win").show();
 	}
 }
 
@@ -173,34 +262,25 @@ function getTile(event, isY){
 // get the 1D cordinate of the event
 function getCord(event, isY){
 	return (isY) ? 
-		event.x - gamediv.offset().left + $(document).scrollLeft() :
-		event.y - gamediv.offset().top + $(document).scrollTop();
+		event.pageX - gamediv.offset().left + $(document).scrollLeft() :
+		event.pageY - gamediv.offset().top + $(document).scrollTop();
 }
 
 // recursive function to clear a open region of the board
 function exposeTile(x, y){
 	board[x][y].state = exposed;
-
+	gamedata.numexposed++;
 	cell = $("#cell-" + x + "-" + y);
-	/*cell.animate({
-	  	backgroundColor: "#36367A",
-		height: cellDim + 2,
-		width: cellDim + 2
-	}, 500, "easeOutBack");
-	cell.animate({
-		height: cellDim,
-		width: cellDim
-	}, 500, "easeOutBack");*/
-	
-	cell.addClass("exposed", 200); 
-
+	cell.addClass("exposed", 0); 
 	if(board[x][y].surrounding == 0){
 		for(var i = -1; i < 2; i++){
 			for(var j = -1; j < 2; j++){
-				if(x + i < 0 || y + j < 0 || x + i > DIM - 1 || y + j > DIM - 1)
+				if(offBoard(x + i, y + j))
 					continue;
-				if(!isBomb(x + i, y + j) && board[x + i][y + j].state == idle)
-					exposeTile(x + i, y + j);
+				if( !isBomb(x + i, y + j) && 
+					board[x + i][y + j].state == idle){
+						exposeTile(x + i, y + j);
+				}
 			}
 		}
 	}
@@ -208,8 +288,9 @@ function exposeTile(x, y){
 
 // called when game is over, exposes every bomb
 function gameover(){
-	for(var i = 0; i < DIM; i++){
-		for(var j = 0; j < DIM; j++){
+	// expose all of the bombs
+	for(var i = 0; i < verticalCellNum; i++){
+		for(var j = 0; j < horizontalCellNum; j++){
 			if(board[i][j].isBomb){
 				board[i][j].state = exposed;
 				cell = $("#cell-" + i + "-" + j);
@@ -219,18 +300,20 @@ function gameover(){
 			}
 		}
 	}
-
+	
 	$("#game-message").fadeIn("slow");
+	$("#game-over").show();
+	$("#instructions").hide();
+	$("#win").hide();
 }
 
-// draws the cells and any letters inside of the cell
+// creates an html string called 'game' and inserts it into the game container then some size assurance
 function drawCells(){
 	gamediv.empty();
 	var gamestring = "";
-
-	for(var i = 0; i < DIM; i++){
+	for(var i = 0; i < verticalCellNum; i++){
 		gamestring += '<div class="game-row">';
-		for(var j = 0; j < DIM; j++){
+		for(var j = 0; j < horizontalCellNum; j++){
 			gamestring += '<div id="cell-' + i + '-' + j + '" class="game-cell';
 			if(board[i][j].state == exposed){
 				if(board[i][j].isBomb)
@@ -239,11 +322,13 @@ function drawCells(){
 					gamestring += ' exposed';
 				gamestring += '">';
 			}else if (board[i][j].state == idle){
-				gamestring += ' idle">';
+				
+					gamestring += ' surround-yes">';
 			}else{
 				gamestring += ' flagged">';
 			}
-			gamestring += '<p>' + board[i][j].surrounding + '</p>';
+			if(board[i][j].surrounding != 0)
+				gamestring += '<p>' + board[i][j].surrounding + '</p>';
 			gamestring += '</div>';
 		}
 		gamestring += '</div>';
@@ -261,8 +346,8 @@ function drawCells(){
 }
 
 function countSurroundings(){
-	for(var i = 0; i < DIM; i++){
-		for(var j = 0; j < DIM; j++){
+	for(var i = 0; i < verticalCellNum; i++){
+		for(var j = 0; j < horizontalCellNum; j++){
 			processCell(i, j);
 		}
 	}
@@ -285,7 +370,22 @@ function processCell(x, y){
 
 // returns if the tile is a bomb being careful to not go off the board
 function isBomb(x, y){
-	if(x < 0 || y < 0 || x > DIM - 1 || y > DIM - 1)
+	if(offBoard(x, y))
 		return false;
 	return board[x][y].isBomb;
+}
+
+// a check to make sure we are staying within board limits
+function offBoard(x, y){
+	return (x < 0 || y < 0 || x > verticalCellNum - 1 || y > horizontalCellNum - 1)
+}
+
+// rounds down to the nearest specified number
+function roundDown(num, nearest){
+     return num - num % nearest;
+}
+
+// returns a random value between specified values
+function randomVal(lower, higher){
+	return Math.floor(Math.random()*(higher - lower + 1) + lower);
 }
